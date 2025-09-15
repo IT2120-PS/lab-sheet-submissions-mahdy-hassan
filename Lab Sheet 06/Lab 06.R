b0950f0037bed575c4c3ffdd2f69fbb6666b3593
mahdy-hassan <IT24100125@my.sliit.lk>
2025-09-15 17:01:14 +0530

    Add files via upload

---
+++ Lab Sheet 06/Lab 06.R
@@ -0,0 +1,15 @@
+getwd()
+setwd("C:\\Users\\IT24100125\\Desktop\\IT24100125 Lab 06")
+getwd()
+
+#Exercise
+#part1
+# i. Biominal distribution
+# ii.
+1-pbinom(46,50,0.85,lower.tail = TRUE)
+
+#part2
+# i.The number of calls received in an hour 
+# ii.Poisson Distribution
+# iii.
+dpois(15,12)
